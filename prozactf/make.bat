fbd50cf83dce20baaac0a22f9fa2e6cbb0ebae4b
PhrostByte <PhrostByte@e3df6df6-840f-0410-bcf8-de91a722e716>
2007-05-23 11:00:44 +0000

    Removed file/folder

--- prozactf/make.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ------------------------------------------------
-echo This will compile Prozac CustomTF
-echo ------------------------------------------------
-fteqcc -O2 -Fhashonly
-if errorlevel 1 exit /b
-echo ------------------------------------------------
-echo Prozac CustomTF has been compiled successfully!!
-echo ------------------------------------------------
